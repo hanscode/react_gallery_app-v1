c4c35f607d4fde1f1f54d330dc7573177a8c9e28
Hans Steffens <hans@steffens.co>
2023-10-30 20:46:33 -0500

    Vite Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div class="container">
+        <h1>No content yet</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
